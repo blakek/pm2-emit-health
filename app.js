68daa24e3db353b4b3f7d5436dd423c7671300a4
Aymeric Lavit d'Hautefort <alavit-d@student.42.fr>
2016-06-22 15:22:43 +0200

    changed transmission

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ pmx.initModule({
 
   widget : {
 
-	  logo : 'https://mdn.mozillademos.org/files/3563/HTML5_Logo_128.png',
+    logo : 'https://mdn.mozillademos.org/files/3563/HTML5_Logo_128.png',
 
     theme : ['#262E35', '#1B2228', '#807C7C', '#807C7C'],
 
@@ -34,38 +34,39 @@ pmx.initModule({
     //CPU retrieval
     cpuStats(100, function(error, result) {
 
-    if(error) return console.error('Fail: could not retrieve cpu metrics', error);
+      if(error) return console.error('Fail: could not retrieve cpu metrics', error);
 
-    var cpu = 0;
-    var number_cpu = result.length;
+      var cpu = 0;
+      var number_cpu = result.length;
 
-    result.forEach(function(value) {
-      cpu += value.cpu;
-    });
+      result.forEach(function(value) {
+        cpu += value.cpu;
+      });
 
-    cpu_used = (cpu/number_cpu).toFixed(2);
+      cpu_used = (cpu/number_cpu).toFixed(2);
 
     });
     //Mem retrieval
     var usedMemProc = shelljs.exec('cat /proc/meminfo | head -5', { async : true, silent:true}, function(err, out) {
 
-    if (err) return console.error('Fail: could not retrieve mem metrics', err);
+      if (err) return console.error('Fail: could not retrieve mem metrics', err);
 
-    var result_memory = (out.match(/\d+/g));
-    var total_mem = result_memory[0];
-    free_mem = parseInt(result_memory[1]) + (parseInt(result_memory[3]) + parseInt(result_memory[4]));
-    var total_mem_gb = (total_mem/1024/1024).toFixed(1) + 'GB';
-    var free_mem_pour = (100 * (1 - free_mem / total_mem)).toFixed(1);
-    mem_used = free_mem_pour;
+      var result_memory = (out.match(/\d+/g));
+      var total_mem = result_memory[0];
+      free_mem = parseInt(result_memory[1]) + (parseInt(result_memory[3]) + parseInt(result_memory[4]));
+      var total_mem_gb = (total_mem/1024/1024).toFixed(1) + 'GB';
+      var free_mem_pour = (100 * (1 - free_mem / total_mem)).toFixed(1);
+      mem_used = free_mem_pour;
     });
-    var status_val = 1400 * (100 - (cpu_used * 2/3 + mem_used * 1/3)) / 100;
+    status_val = 1400 * (100 - (cpu_used * 2/3 + mem_used * 1/3)) / 100;
   }, 1000);
 
-    //POST
-    if (conf.ips !== "")
-      app.use(ipfilter(conf.ips.join(','), {mode: 'allow'}));
-    app.get('/', function(req, res) {
-      res.send(status_val);
-    });
-    app.listen(conf.port);
+  //POST
+  if (conf.ips !== "")
+    app.use(ipfilter(conf.ips.join(','), {mode: 'allow'}));
+  app.get('/', function(req, res) {
+    console.log(status_val);
+    res.json(status_val);
+  });
+  app.listen(conf.port);
 });
